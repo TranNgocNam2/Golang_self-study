badb8d410c0a006d748a0abc2dddc364083f5202
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2023-12-29 13:55:45 +0700

    Single-record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"snippetbox.nam.net/internal/models"
 )
 
 // Change the signature of the home handler so it is defined as a method against
@@ -68,9 +71,23 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Use the SnippetModel object's Get method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// return a 404 Not Found response
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
 	// Use the fmt.Fprintf() function to interpolate the id value with our response
 	// and write it to the http.ResponseWriter
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// Write the snippet data as a plain-text HTTP response body.
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Add a snippetCreate handler function.
