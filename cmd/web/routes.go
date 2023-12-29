8faeec88d7db48455e5e30e0175435c8495ed716
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2023-12-30 01:15:13 +0700

    Composable middleware chains

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/justinas/alice"
 )
 
 // The routes() method returns a servemux containing our application routes.
@@ -14,6 +16,9 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	// Wrap the existing chain with the logRequest middleware.
-	return app.logRequest(secureHeaders(mux))
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
