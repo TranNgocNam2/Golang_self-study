10637fe54b0ef975a4cca6f18e2a5b9451325385
TranNgocNam2 <ngocnamsieuquay@gmail.com>
2024-01-08 01:16:59 +0700

    Testing HTML forms

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -19,6 +19,6 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 func StringContains(t *testing.T, actual, expectedSubstring string) {
 	t.Helper()
 	if !strings.Contains(actual, expectedSubstring) {
-		t.Errorf("gotL %q; expected to contain %q", actual, expectedSubstring)
+		t.Errorf("got %q; expected to contain %q", actual, expectedSubstring)
 	}
 }
